910e9f3be2933168ce54324fd54722ce272fe9ce
DUCDEPTRAINO1 <20119336@student.hcmute.edu.vn>
2023-09-19 15:47:14 +0700

    add A

--- app/src/main/java/com/example/test_sourcetree/MainActivity.java
+++ app/src/main/java/com/example/test_sourcetree/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String text = "toi la Thai";
-        String text1 = "toi la anh Thai1";
+        String text = "toi la zDuc DUc ";
+        String text1 = "toi la Duc Duc";
     }
 }
